a97269630cb657874daa0717a4efe353cfdd70fc
小殊 <caofei@jucaicat.com>
2018-10-20 18:44:22 +0800

    提交代码

---
+++ netty-research/src/main/java/com/strong/fee/research/NettyApplication.java
@@ -0,0 +1,16 @@
+package com.strong.fee.research;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * author: 小殊
+ * create: 2018/10/20 下午4:17
+ */
+@SpringBootApplication
+public class NettyApplication {
+
+    public static void main(String args[]) {
+        SpringApplication.run(NettyApplication.class, args);
+    }
+}
---
+++ spring-boot-ds/src/main/java/com/strong/fee/ds/DataSourceApplication.java
@@ -0,0 +1,15 @@
+package com.strong.fee.ds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * author: 小殊
+ * create: 2018/9/27 上午10:17
+ */
+@SpringBootApplication
+public class DataSourceApplication {
+    public static void main(String args[]) {
+        SpringApplication.run(DataSourceApplication.class, args);
+    }
+}
